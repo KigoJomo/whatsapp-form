7d5390ca70dd5a6d8f419b4f65b8c1de26456cfa
KigoJomo <kigojomo@gmail.com>
2024-10-07 20:42:47 +0300

    Update

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={"w-full"} style={{ height: "100dvh" }}>
-      <body className="h-screen gradient-background flex flex-col items-center justify-center">
+      <body className="h-full gradient-background flex flex-col items-center justify-center">
         {children}
       </body>
     </html>
